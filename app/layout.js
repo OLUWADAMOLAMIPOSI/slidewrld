04f211195407d790d2db03ac2b9d31211409bce9
OLUWADAMOLAMIPOSI <successweb49@gmail.com>
2026-09-21 13:01:01 +0100

    Force dynamic rendering across all pages

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import "./globals.css";
 import { CartProvider } from "@/components/CartContext";
 import { readData } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
